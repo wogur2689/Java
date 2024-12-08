7938f1b4f914a9596b374fc718f783221b1e82ad
jaehyeok_gil <81075170+wogur2689@users.noreply.github.com>
2024-12-08 13:57:03 +0900

    Create dto.java

---
+++ dto.java
@@ -0,0 +1,9 @@
+//계층 간 데이터를 전달하기 위해 사용하는 객체
+//특징 : 속성 집합, 가변성
+public class UserDTO {
+    private String name;
+    private String email;
+    private int age;
+
+    // Constructors, getters, setters
+}
